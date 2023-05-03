658c16b75e8cedaceddee926722221d8614a6cb3
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2023-05-03 17:52:38 -0600

    update futrapido

--- src/componentes/MenuAdmin.js
+++ src/componentes/MenuAdmin.js
@@ -16,7 +16,10 @@ class MenuAdmin extends React.Component  {
       return (
         <Menu >
           <Link id="listaEscuela" className="menu-item" to="/lista-escuelas"> <FaUserPlus className='iconMenu'/>  Lista de Escuelas</Link>
-          <Link id="estadisticas" className="menu-item" to="/" > <FaRegChartBar  className='iconMenu'/>  Estadisticas</Link>
+          <Link id="estadisticas" className="menu-item" to="/estadisticas-fr" > <FaRegChartBar  className='iconMenu'/>  Tabla General de Fútbol Rápido </Link>
+          <Link id="estadisticas" className="menu-item" to="/RR-FR-FemenilSuperior" > <FaRegChartBar  className='iconMenu'/>  Registrar Resultados Fútbol Rápido Femenil Nivel Superior </Link>
+          <Link id="estadisticas" className="menu-item" to="/registrar-resultado" > <FaRegChartBar  className='iconMenu'/>  Registrar Resultados Fútbol Rápido Femenil Media Superior </Link>
+          <Link id="estadisticas" className="menu-item" to="/registrar-resultado" > <FaRegChartBar  className='iconMenu'/>  Registrar Resultados Fútbol Asociación </Link>
           <Link id="rolJuegos" className="menu-item" to="/nuevo-torneo" > <FaCalendarAlt  className='iconMenu'/>  Nuevo Torneo </Link>
           <Link id="rolJuegos" className="menu-item" to="/lista-torneos" > <FaCalendarAlt  className='iconMenu'/>  Lista de Torneos </Link>
           <Link id="noticias" className="menu-item" to="/noticias" > <FaNewspaper  className='iconMenu'/>  Noticias</Link>
