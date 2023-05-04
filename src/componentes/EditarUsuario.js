229e3647ac1ec5b11d36fbe20ab6ce69d06c7759
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2023-05-03 23:49:50 -0600

    update editar usuario

--- src/componentes/EditarUsuario.js
+++ src/componentes/EditarUsuario.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import RegistroUsuario from './RegistroUsuario';
 import useObtenerUsuario from '../hooks/useObtenerUsuario';
 import BtnRegresar from '../elementos/BtnRegresar';
-import {ContenedorBotonCentral} from '../elementos/ElementosDeLista';
+import { ContenedorBotonCentral } from '../elementos/ElementosDeLista';
 
 const EditarUsuario = () => {
-   
-    const{id} = useParams();
-    const[usuario] = useObtenerUsuario(id);
-    
-    
+    const { id } = useParams();
+    const [usuario] = useObtenerUsuario(id);
+    console.log({usuario});
+
     return (
         <>
-        <Helmet>
-            <title>Editar Usuario</title>
-        </Helmet> 
-        <RegistroUsuario usuario={usuario}/>
-        <ContenedorBotonCentral>
-            <BtnRegresar ruta={usuario.rol === "jugador" ? '/menu-profe' : '/rol'} />
-        </ContenedorBotonCentral>        
+            <Helmet>
+                <title>Editar Usuario</title>
+            </Helmet> 
+            <RegistroUsuario usuario={usuario} />
+            <ContenedorBotonCentral>
+                <BtnRegresar ruta={usuario.rol === "jugador" ? '/menu-profe' : '/rol'} />
+            </ContenedorBotonCentral>        
         </>
      );
 }
