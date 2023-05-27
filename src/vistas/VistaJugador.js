d549d2b591c1e2f29fe1acd069e8bad7558b52bc
sandraSuarezP <ssuarezp1400@alumno.ipn.mx>
2023-05-26 18:35:15 -0600

    Pagina FB en Vistas de los usuarios

--- src/vistas/VistaJugador.js
+++ src/vistas/VistaJugador.js
@@ -71,8 +71,7 @@ const VistaJugador = () => {
             {/* Contenido de las diapositivas */}
           </Carousel>
           <div className="twitter-timeline" style={{ width: '500px', marginLeft: '20px' }}>
-          <iframe id="twitter-widget-0" scrolling="no" allowTransparency="true" allowFullScreen="true" class="" style={{position: "static", visibility: "visible", width: "700px", height: "700px", display: "block-inline", flexGrow: "1"}} title="Twitter Timeline" src="https://syndication.twitter.com/srv/timeline-profile/screen-name/escomunidad?dnt=false&amp;embedId=twitter-widget-0&amp;frame=false&amp;hideBorder=false&amp;hideFooter=false&amp;hideHeader=false&amp;hideScrollBar=false&amp;lang=es&amp;maxHeight=500px&amp;origin=https%3A%2F%2Fwww.escom.ipn.mx%2F&amp;sessionId=a69e2f6859857ad06c67a4deebf8c794f3dff3d6&amp;showHeader=true&amp;showReplies=false&amp;siteScreenName=escomunidad&amp;transparent=false&amp;widgetsVersion=1c23387b1f70c%3A1664388199485" frameBorder="0"></iframe>
-            <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+          <iframe title="Facebook Timeline" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIPN.FUTBOL&amp;tabs=timeline&amp;width=500&amp;height=450&amp;small_header=false&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=false&amp;appId" width="500" height="420" style={{border:"none", overflow:"hidden"}} scrolling="no" frameBorder="0" allowFullScreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
           </div>
         </div>
       </div>
